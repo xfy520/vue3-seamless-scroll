33ba5c509d063e7388f9ad42b8d70c37782e39ab
xfy520 <longzinziyan@gmail.com>
2025-01-08 22:32:43 +0800

    feat：新增移除元素

--- package/VerticalScroll.vue
+++ package/VerticalScroll.vue
@@ -80,59 +80,88 @@ export default defineComponent({
     */
     const realWrapperHiddenRef = ref(null);
 
-    const bufferSize = ref(0);
-
     const funArgs = ref([]);
-
-    const targetList = listMap(props.list);
-
-    const cursorIndex = ref(-1);
-
-    const realBoxHeight = ref(0);
 
     const offset = ref(0);
 
-    const childrenHeightList = ref([]);
-
-    const bufferTotalHeight = ref(0);
-
-    const reqFrame = ref(null);
-
     const isHover = ref(false);
-
-    const singleState = ref(false);
-
-    const singleOffset = ref(0);
-
-    const tempOffset = ref(0);
-
-    const listCanScroll = ref(false);
-
-    const skipUpdate = ref(false);
 
     const isScroll = computed(() => props.hover ? !isHover.value && props.modelValue : props.modelValue);
 
     const testList = ref(listMap(props.list.slice(0, props.visibleCount)));
 
+    const targetList = listMap(props.list);
+
+    let childrenHeightList = [];
+
+    let bufferTotalHeight = 0;
+
+    let bufferSize = 0;
+
+    let cursorIndex = -1;
+
+    let realBoxHeight = 0;
+
+    let reqFrame = null;
+
+    let singleState = false;
+
+    let singleOffset = 0;
+
+    let tempOffset = 0;
+
+    let listCanScroll = false;
+
+
     const cancle = () => {
-      cancelAnimationFrame(reqFrame.value);
-      reqFrame.value = null;
+      cancelAnimationFrame(reqFrame);
+      reqFrame = null;
+    }
+
+    const updateCursorIndex = () => {
+      const totalIndex = cursorIndex + bufferSize;
+      if (totalIndex >= targetList.length) {
+        const tempIndex = totalIndex - targetList.length;
+        const tempFuns = ['splice',
+          [0, bufferSize],
+          [cursorIndex],
+          [0, tempIndex]];
+        funArgs.value = tempFuns;
+        if (tempIndex > targetList.length) {
+          cursorIndex = targetList.length;
+        } else {
+          cursorIndex = tempIndex;
+        }
+      } else {
+        const tempFuns = ['splice', [0, bufferSize], [cursorIndex, totalIndex]];
+        funArgs.value = tempFuns;
+        cursorIndex = totalIndex;
+      }
+    }
+
+    const initHeight = () => {
+      const children = Array.from(realWrapperRef.value.children);
+      const subChildren = props.upScroll ?
+        children.slice(0, bufferSize) :
+        children.slice(visibleItems.value.length - bufferSize, visibleItems.value.length);
+      childrenHeightList = subChildren.map((c) => c.offsetHeight);
+      bufferTotalHeight = childrenHeightList
+        .reduce((a, b) => a + b, 0);
     }
 
     const animation = (isWheel, isUp, speed) => {
       cancle();
-
-      if (!listCanScroll.value) {
+      if (!listCanScroll) {
         return;
       }
 
       const scrollFun = () => {
-        if (props.singleLine && singleOffset.value >= childrenHeightList.value[0]) {
-          singleState.value = true;
-          childrenHeightList.value.shift();
-          singleOffset.value = 0;
+        if (props.singleLine && singleOffset >= childrenHeightList[0]) {
+          singleState = true;
+          childrenHeightList.shift();
+          singleOffset = 0;
           setTimeout(() => {
-            singleState.value = false;
+            singleState = false;
             if (!isWheel) {
               animation(false, props.upScroll, props.speed);
             }
@@ -143,45 +172,21 @@ export default defineComponent({
           }
         }
       }
-      if (isScroll.value && !singleState.value || isWheel) {
-        reqFrame.value = requestAnimationFrame(() => {
-          tempOffset.value += speed;
-          singleOffset.value += speed;
+      if (isScroll.value && !singleState || isWheel) {
+        reqFrame = requestAnimationFrame(() => {
+          tempOffset += speed;
+          singleOffset += speed;
           if (isUp) {
             offset.value += speed;
           } else {
             offset.value -= speed;
           }
-          if (tempOffset.value > bufferTotalHeight.value) {
-            const totalIndex = cursorIndex.value + bufferSize.value;
-            if (totalIndex >= targetList.length) {
-              const tempIndex = totalIndex - targetList.length;
-              const tempFuns = ['splice',
-                [0, bufferSize.value],
-                [cursorIndex.value],
-                [0, tempIndex]];
-              funArgs.value = tempFuns;
-              if (tempIndex > targetList.length) {
-                cursorIndex.value = targetList.length;
-              } else {
-                cursorIndex.value = tempIndex;
-              }
-            } else {
-              const tempFuns = ['splice', [0, bufferSize.value], [cursorIndex.value, totalIndex]];
-              funArgs.value = tempFuns;
-              cursorIndex.value = totalIndex;
-            }
+          if (tempOffset > bufferTotalHeight) {
+            updateCursorIndex();
             nextTick(() => {
-              tempOffset.value = 0;
-              offset.value = isUp ? 0 : realBoxHeight.value;
-
-              const children = isUp ? Array.from(realWrapperRef.value.children)
-                .slice(0, bufferSize.value) : Array.from(realWrapperRef.value.children)
-                  .slice(visibleItems.value.length - bufferSize.value, visibleItems.value.length);
-              childrenHeightList.value = children.map((children) => children.offsetHeight);
-              bufferTotalHeight.value = childrenHeightList.value
-                .reduce((a, b) => a + b, 0);
-
+              offset.value = props.upScroll ? 0 : getFullHeight() - realBoxHeight;
+              tempOffset = 0;
+              initHeight();
               scrollFun();
             })
           } else {
@@ -221,10 +226,6 @@ export default defineComponent({
      * @type {import('vue').ComputedRef<Array<any>>}
      */
     const visibleItems = computed(() => {
-      if (skipUpdate.value) {
-        skipUpdate.value = false;
-        return visibleItems.value;
-      }
       let tempList = [];
       if (funArgs.value.length === 0) {
         tempList = targetList.slice(0, props.visibleCount);
@@ -239,168 +240,212 @@ export default defineComponent({
           tempList.push(...targetList.slice(...args));
         });
       }
-      console.log('---', funArgs.value, '===', tempList);
+      console.log('---', funArgs.value, '===', tempList, '---', bufferTotalHeight);
       if (!props.upScroll) {
         tempList.reverse();
       }
       return duplicateId(tempList);
     });
 
-
     const initCursorIndex = () => {
-      cursorIndex.value = props.visibleCount + bufferSize.value;
-      if (cursorIndex.value >= targetList.length) {
-        const tempIndex = cursorIndex.value - targetList.length;
-        const tempFunArgs = ['slice', [0, cursorIndex.value], [0, tempIndex]];
+      cursorIndex = props.visibleCount + bufferSize;
+      if (cursorIndex >= targetList.length) {
+        const tempIndex = cursorIndex - targetList.length;
+        const tempFunArgs = ['slice', [0, cursorIndex], [0, tempIndex]];
         if (tempIndex > targetList.length) {
-          cursorIndex.value = targetList.length;
+          cursorIndex = targetList.length;
         } else {
-          cursorIndex.value = tempIndex;
+          cursorIndex = tempIndex;
         }
         return tempFunArgs;
       } else {
-        return ['slice', [0, cursorIndex.value]];
+        return ['slice', [0, cursorIndex]];
       }
     }
 
-    const initHeight = (element) => {
-      const children = Array.from(element.children);
-      const subChildren = props.upScroll ?
-        children.slice(0, bufferSize.value) :
-        children.slice(visibleItems.value.length - bufferSize.value, visibleItems.value.length);
-      childrenHeightList.value = subChildren.map((c) => c.offsetHeight);
-      bufferTotalHeight.value = childrenHeightList.value
-        .reduce((a, b) => a + b, 0);
-    }
 
-    const resetData = () => {
-      bufferSize.value = 0;
-      funArgs.value = [];
-      listCanScroll.value = false;
+    const getFullHeight = () => {
+      return Array.from(realWrapperRef.value.children)
+        .map((c) => c.offsetHeight).reduce((a, b) => a + b, 0);
     }
 
     onMounted(() => {
       realBoxRef.value.addEventListener('mouseenter', onMouseenter);
       realBoxRef.value.addEventListener('mouseleave', onMouseleave);
       realBoxRef.value.addEventListener('wheel', onWheel);
-      realBoxHeight.value = realBoxRef.value.offsetHeight;
-
-      offset.value = props.upScroll ? 0 : realBoxHeight.value;
-
+      realBoxHeight = realBoxRef.value.offsetHeight;
       nextTick(() => {
-        const hasVerticalScroll = realWrapperHiddenRef.value.offsetHeight > realBoxHeight.value;
+        const hasVerticalScroll = realWrapperHiddenRef.value.offsetHeight > realBoxHeight;
         if (hasVerticalScroll) {
           let tempBufferSize = Math.max(1, targetList.length - props.visibleCount);
-          bufferSize.value = Math.min(5, tempBufferSize);
+          bufferSize = Math.min(5, tempBufferSize);
           funArgs.value = initCursorIndex();
-          initHeight(realWrapperHiddenRef.value);
-          listCanScroll.value = true;
-          animation(false, props.upScroll, props.speed);
+          nextTick(() => {
+            offset.value = props.upScroll ? 0 : getFullHeight() - realBoxHeight;
+
+            initHeight();
+            listCanScroll = true;
+            animation(false, props.upScroll, props.speed);
+          })
         } else {
-          resetData();
+          init();
         }
         testList.value = [];
       });
     });
 
-    const reset = () => {
-      offset.value = props.upScroll ? 0 : realBoxHeight.value;
-      tempOffset.value = 0;
-      singleOffset.value = 0;
+    const init = () => {
+      listCanScroll = false;
+      bufferSize = 0;
+      funArgs.value = [];
+      nextTick(() => {
+        offset.value = props.upScroll ? 0 : getFullHeight() - realBoxHeight;
+        tempOffset = 0;
+        singleOffset = 0;
+      });
     }
 
-    const add = (index, value) => {
-      if (index > targetList.length) {
-        index = targetList.length;
-      }
-
-      const findIndexs = [];
-
-      visibleItems.value.forEach((v, i) => {
-        if (v.index === index) {
-          findIndexs.push(i);
+    const add = (index, values) => {
+      if (!!values && values.length > 0) {
+        if (index > targetList.length) {
+          index = targetList.length;
         }
-      });
+        if (index < 0) {
+          index = 0;
+        }
+        const findIndexs = [];
+        if (values.length === 1) {
+          visibleItems.value.forEach((v, i) => {
+            if (v.index === index) {
+              findIndexs.push(i);
+            }
+          });
+        }
+        const datas = [];
+        values.forEach((v) => {
+          datas.push({
+            id: uuid(),
+            data: v
+          });
+        })
 
-      const data = {
-        id: uuid(),
-        index: index,
-        data: value
-      }
+        targetList.splice(index, 0, ...datas);
+        targetList.forEach((v, i) => {
+          v.index = i;
+        });
 
-      targetList.splice(index, 0, data);
+        let tempBufferSize = targetList.length - props.visibleCount;
+        tempBufferSize = Math.max(1, tempBufferSize);
+        tempBufferSize = Math.min(5, tempBufferSize);
 
-      targetList.forEach((v, i) => {
-        v.index = i;
-      });
-
-      let tempBufferSize = targetList.length - props.visibleCount;
-      tempBufferSize = Math.max(1, tempBufferSize);
-      tempBufferSize = Math.min(5, tempBufferSize);
-
-      if (listCanScroll.value) {
-        if (index < cursorIndex.value) {
-          cursorIndex.value += 1;
-          if (cursorIndex.value > targetList.length) {
-            cursorIndex.value = 0;
+        if (listCanScroll) {
+          if (index < cursorIndex) {
+            cursorIndex += 1;
+            if (cursorIndex > targetList.length) {
+              cursorIndex = 0;
+            }
           }
-        }
 
-        if (findIndexs.length > 0) {
-          findIndexs.forEach((i) => {
-            visibleItems[i] = data;
+          if (values.length === 1 && findIndexs.length > 0) {
+            findIndexs.forEach((i) => {
+              visibleItems[i] = datas[0];
+            })
+          }
+          if (tempBufferSize !== bufferSize) {
+            bufferSize = tempBufferSize;
+          }
+        } else {
+          testList.value = targetList.slice(0, props.visibleCount);
+          nextTick(() => {
+            const hasVerticalScroll = realWrapperHiddenRef.value.offsetHeight > realBoxHeight;
+            if (hasVerticalScroll) {
+              bufferSize = tempBufferSize;
+              funArgs.value = initCursorIndex();
+              nextTick(() => {
+                offset.value = props.upScroll ? 0 : getFullHeight() - realBoxHeight;
+                tempOffset = 0;
+                singleOffset = 0;
+
+                initHeight();
+                listCanScroll = true;
+                animation(false, props.upScroll, props.speed);
+              });
+            } else {
+              init();
+            }
+            testList.value = [];
           })
         }
-        if (tempBufferSize !== bufferSize.value) {
-          bufferSize.value = tempBufferSize;
-        }
-      } else {
-        testList.value = targetList.slice(0, props.visibleCount);
-        nextTick(() => {
-          const hasVerticalScroll = realWrapperHiddenRef.value.offsetHeight > realBoxHeight.value;
-          if (hasVerticalScroll) {
-            bufferSize.value = tempBufferSize;
-            funArgs.value = initCursorIndex();
-            reset();
-            initHeight(realWrapperHiddenRef.value);
-            listCanScroll.value = true;
-            animation(false, props.upScroll, props.speed);
-          } else {
-            reset();
-            resetData();
-          }
-          testList.value = [];
-        })
       }
     }
 
     const remove = (index) => {
-      const list = [...targetData.value.list];
-      if (index >= 0 && index < list.length) {
-        list.splice(index, 1);
-        if (listCanScroll.value) {
-          testList.value = list.slice(0, props.visibleCount);
+      if (index >= 0 && index < targetList.length) {
+        targetList.splice(index, 1);
+        if (listCanScroll) {
+          testList.value = targetList.slice(0, props.visibleCount);
           nextTick(() => {
-            const hasVerticalScroll = realWrapperHiddenRef.value.offsetHeight > realBoxHeight.value;
+            const hasVerticalScroll = realWrapperHiddenRef.value.offsetHeight > realBoxHeight;
             if (hasVerticalScroll) {
-              let tempBufferSize = list.length - props.visibleCount;
+              let tempBufferSize = targetList.length - props.visibleCount;
               tempBufferSize = Math.max(1, tempBufferSize);
               tempBufferSize = Math.min(5, tempBufferSize);
-
-              if (tempBufferSize !== bufferSize.value) {
-                bufferSize.value = tempBufferSize;
+              if (tempBufferSize !== bufferSize) {
+                bufferSize = tempBufferSize;
               }
-
+              const findIndexs = [];
+              visibleItems.value.forEach((v, i) => {
+                if (v.index === index) {
+                  findIndexs.push(i);
+                }
+              });
+              if (findIndexs.length > 0) {
+                if (funArgs.value[0] === 'splice') {
+                  updateCursorIndex();
+                  nextTick(() => {
+                    initHeight();
+                  });
+                } else {
+                  funArgs.value = initCursorIndex();
+                  nextTick(() => {
+                    initHeight();
+                  });
+                }
+              }
             } else {
-              reset();
-              resetData(list);
+              init();
             }
             testList.value = [];
           })
         } else {
-
+          init();
         }
       }
+    }
+
+
+    const reset = () => {
+      nextTick(() => {
+        testList.value = targetList.slice(0, props.visibleCount);
+        nextTick(() => {
+          const hasVerticalScroll = realWrapperHiddenRef.value.offsetHeight > realBoxHeight;
+          if (hasVerticalScroll) {
+            if (funArgs.value[0] === 'splice') {
+              updateCursorIndex();
+              nextTick(() => {
+                initHeight();
+              });
+            } else {
+              funArgs.value = initCursorIndex();
+              nextTick(() => {
+                initHeight();
+              });
+            }
+          } else {
+            init();
+          }
+        });
+      })
     }
 
 
@@ -426,10 +471,10 @@ export default defineComponent({
       }
     }
 
-    expose({ add, remove, update });
+    expose({ add, remove, update, reset });
 
     onUnmounted(() => {
-      cancelAnimationFrame(reqFrame.value);
+      cancelAnimationFrame(reqFrame);
       if (realBoxRef.value) {
         realBoxRef.value.removeEventListener('mouseenter', onMouseenter);
         realBoxRef.value.removeEventListener('mouseleave', onMouseleave);
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="scroll">
+  <!-- <div class="scroll">
     <div>默认配置</div>
     <VerticalScroll :list="list" :speed="0.3" :visibleCount="10" ref="verticalScrollRef">
       <template v-slot="{ data, index }">
@@ -7,30 +7,57 @@
           <template v-if="data.id % 2 === 0">
             <div>{{ data.name }}</div>
           </template>
-          <template v-else>
+<template v-else>
             <div>{{ data.name }}</div>
             <div>{{ data.name }}</div>
           </template>
-          <!-- <div>{{ data.name }}</div> -->
-        </span>
-      </template>
-    </VerticalScroll>
-  </div>
-  <div class="scroll">
-    <div>快速</div>
-    <div>
-      <VerticalScroll :list="list" :speed="1" :visibleCount="12">
-        <template v-slot="{ data, index }">
+</span>
+</template>
+</VerticalScroll>
+</div>
+<div class="scroll">
+  <div>快速</div>
+  <div>
+    <VerticalScroll :list="list" :speed="1" :visibleCount="12">
+      <template v-slot="{ data, index }">
           <div style="width: 100%; line-height: 30px;">
             {{ data.name }}
           </div>
         </template>
-      </VerticalScroll>
-    </div>
+    </VerticalScroll>
   </div>
+</div>
+<div class="scroll">
+  <div>单步</div>
+  <VerticalScroll :list="list" :speed="1" singleLine :visibleCount="10" ref="verticalScrollRef">
+    <template v-slot="{ data, index }">
+        <span style="width: 100%; display: block; line-height: 30px;">
+          <template v-if="data.id % 2 === 0">
+            <div>{{ data.name }}</div>
+          </template>
+    <template v-else>
+            <div>{{ data.name }}</div>
+            <div>{{ data.name }}</div>
+          </template>
+    </span>
+    </template>
+  </VerticalScroll>
+</div>
+<div class="scroll">
+  <div>单步加单步时间</div>
+  <div>
+    <VerticalScroll :list="list" :speed="1" singleLine :singleWaitTime="3000" :visibleCount="12">
+      <template v-slot="{ data, index }">
+          <div style="width: 100%; line-height: 30px;">
+            {{ data.name }}
+          </div>
+        </template>
+    </VerticalScroll>
+  </div>
+</div> -->
   <div class="scroll">
-    <div>单步</div>
-    <VerticalScroll :list="list" :speed="1" singleLine :visibleCount="10" ref="verticalScrollRef">
+    <div>向上</div>
+    <VerticalScroll :list="list" :speed="0.2" :visibleCount="6" ref="verticalScrollRef">
       <template v-slot="{ data, index }">
         <span style="width: 100%; display: block; line-height: 30px;">
           <template v-if="data.id % 2 === 0">
@@ -40,43 +67,13 @@
             <div>{{ data.name }}</div>
             <div>{{ data.name }}</div>
           </template>
-          <!-- <div>{{ data.name }}</div> -->
-        </span>
-      </template>
-    </VerticalScroll>
-  </div>
-  <div class="scroll">
-    <div>单步加单步时间</div>
-    <div>
-      <VerticalScroll :list="list" :speed="1" singleLine :singleWaitTime="3000" :visibleCount="12">
-        <template v-slot="{ data, index }">
-          <div style="width: 100%; line-height: 30px;">
-            {{ data.name }}
-          </div>
-        </template>
-      </VerticalScroll>
-    </div>
-  </div>
-  <div class="scroll">
-    <div>向下</div>
-    <VerticalScroll :list="list" :speed="2" :visibleCount="10" ref="verticalScrollRef" :upScroll="false">
-      <template v-slot="{ data, index }">
-        <span style="width: 100%; display: block; line-height: 30px;">
-          <template v-if="data.id % 2 === 0">
-            <div>{{ data.name }}</div>
-          </template>
-          <template v-else>
-            <div>{{ data.name }}</div>
-            <div>{{ data.name }}</div>
-          </template>
-          <!-- <div>{{ data.name }}</div> -->
         </span>
       </template>
     </VerticalScroll>
   </div>
 </template>
 <script lang="ts">
-const listData = Array.from({ length: 10000 }, (_, i) => ({
+const listData = Array.from({ length: 10 }, (_, i) => ({
   id: Date.now() + i + 1,
   name: `Vue3.0 无缝滚动组件展示数据第 ${i + 1} 条`,
 }));
@@ -92,27 +89,27 @@ export default defineComponent({
     const list = ref(listData);
     const verticalScrollRef = ref();
     onMounted(() => {
-      setTimeout(() => {
-        verticalScrollRef.value.add(2, {
-          id: 5,
-          name: `Vue3.0 无缝滚动组件展示数据第 ${Date.now()} 条`
-        })
-      }, 1000);
-      setTimeout(() => {
-        verticalScrollRef.value.add(5, {
-          id: 6,
-          name: `Vue3.0 无缝滚动组件展示数据第 ${Date.now()} 条`
-        })
-      }, 2000);
-      setTimeout(() => {
-        verticalScrollRef.value.add(3, {
-          id: 999,
-          name: `Vue3.0 无缝滚动组件展示数据第 ${Date.now()} 条`
-        })
-      }, 5000);
       // setTimeout(() => {
-      //   verticalScrollRef.value.remove(3);
+      //   verticalScrollRef.value.add(2, {
+      //     id: 5,
+      //     name: `Vue3.0 无缝滚动组件展示数据第 ${Date.now()} 条`
+      //   })
       // }, 1000);
+      // setTimeout(() => {
+      //   verticalScrollRef.value.add(5, {
+      //     id: 6,
+      //     name: `Vue3.0 无缝滚动组件展示数据第 ${Date.now()} 条`
+      //   })
+      // }, 2000);
+      // setTimeout(() => {
+      //   verticalScrollRef.value.add(3, {
+      //     id: 999,
+      //     name: `Vue3.0 无缝滚动组件展示数据第 ${Date.now()} 条`
+      //   })
+      // }, 5000);
+      setTimeout(() => {
+        verticalScrollRef.value.remove(6);
+      }, 10000);
       // setTimeout(() => {
       //   verticalScrollRef.value.update(1,
       //     {
@@ -136,7 +133,7 @@ export default defineComponent({
 }
 
 .scroll>div:last-child {
-  height: 300px;
+  height: 180px;
   width: 390px;
   margin: 0 25px;
   overflow: hidden;
