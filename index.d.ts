3bbaf16c958bb00b2a0102bdfe13daf9818b2623
xfy520 <78788091+xfy520@users.noreply.github.com>
2026-09-02 14:09:57 +0800

    Refactor OptionsType to use primitive types

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,24 @@
-import { ComputedGetter, WritableComputedOptions } from 'vue';
-
 type OptionsType = {
-  modelValue?: ComputedGetter<boolean>;
-  list: ComputedGetter<Array<unknown>>;
-  step?: ComputedGetter<number>;
-  hover?: ComputedGetter<boolean>;
-  direction?: ComputedGetter<"up" | "down" | "left" | "right">;
-  singleWaitTime?: ComputedGetter<number>;
-  ease?: ComputedGetter<"ease-in" | "linear" | "ease" | "ease-out" | "ease-in-out" | string>;
-  wheel?: ComputedGetter<boolean>;
-  visibleCount?: ComputedGetter<number>;
-  singleLine?: ComputedGetter<boolean>;
+  modelValue?: boolean;
+  list: Array<unknown>;
+  step?: number;
+  hover?: boolean;
+  direction?: 'up' | 'down' | 'left' | 'right';
+  singleWaitTime?: number;
+  delay?: number;
+  ease?: string;
+  wheel?: boolean;
+  visibleCount?: number;
+  singleLine?: boolean;
 }
 
 declare module 'vue3-seamless-scroll' {
-  export const Vue3SeamlessScroll: import('vue').DefineComponent < OptionsType, OptionsType, OptionsType, OptionsType, OptionsType,
-    OptionsType, OptionsType, OptionsType, string, OptionsType, OptionsType, OptionsType >
+  export const Vue3SeamlessScroll: import('vue').DefineComponent < OptionsType, {}, any >
+  export const VerticalScroll: import('vue').DefineComponent < OptionsType, {}, any >
+  export const HorizontalScroll: import('vue').DefineComponent < OptionsType, {}, any >
 
-  const install: (app: import('vue').App, options: {
-    name: string
-  }) => unknown;
+  const install: (app: import('vue').App, options?: {
+    name?: string
+  }) => void;
   export default install;
 }
